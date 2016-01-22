93636d430a30787c6e252379f2d49386a7ba0a45
alvaro <alraled@hotmail.es>
2016-01-22 14:24:50 +0100

    Funciones como variables

---
+++ Funciones como Variables/funciones.js
@@ -0,0 +1,17 @@
+﻿var saludo = function (texto) {
+    
+    alert("Hola " + texto.target.outerText);
+}
+
+function saludando (texto) {
+
+    alert("Hola " + texto.target.outerText);
+}
+
+
+document.getElementById("btn1").onclick = function (e) { // Esto es la estructura de una FUNCIÓN ANÓNIMA.//
+    alert("Hola " + e.target.outerText);
+}
+document.getElementById("btn2").onclick = saludando; // Esto es la estructura de cómo hacer para que al clickar en el botón 2, éste arroje un "Hola Botón 2".//
+
+
